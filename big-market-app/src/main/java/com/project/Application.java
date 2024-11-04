720238007261af54e083b89d6f6df72a4eb67d2c
lushihao <lushihao@wjinfo.com>
2024-11-04 14:30:46 +0800

    初始代码

--- big-market-app/src/main/java/com/project/Application.java
+++ big-market-app/src/main/java/com/project/Application.java
@@ -16,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
 
     public static void main(String[] args){
+
         SpringApplication.run(Application.class);
     }
 
